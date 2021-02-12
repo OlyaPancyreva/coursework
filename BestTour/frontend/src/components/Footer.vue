2850a4d0276de7bf5b44c46217a9b582785c7bbb
O'Lik <55744134+OlyaPancyreva@users.noreply.github.com>
2021-02-13 01:00:18 +0300

    Create Footer.vue

---
+++ BestTour/frontend/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card mb-3 img" style="width:1000px; margin-top: 20px; padding:10px; background-color: #0f6890;">
+    <div class="card-body" style="color: white;">
+      <p class="card-text">2020 @Туристическая компания "Best Tour"
+      </p>
+       </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Footer"
+  }
+</script>
+
+<style scoped>
+
+</style>
